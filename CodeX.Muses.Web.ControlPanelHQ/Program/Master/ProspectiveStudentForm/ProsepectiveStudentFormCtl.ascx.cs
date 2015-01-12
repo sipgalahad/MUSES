1c47929f7eee84e94ccd5327e7a7efbc634266c7
qis-agi <qis-agi@medinfras.com>
2015-01-12 10:18:12 +0700

    ProsepectiveStudentForm in ControlPanelHQ

---
+++ CodeX.Muses.Web.ControlPanelHQ/Program/Master/ProspectiveStudentForm/ProsepectiveStudentFormCtl.ascx.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DevExpress.Web.ASPxCallbackPanel;
+using CodeX.Common;
+using CodeX.Web.Common.UI;
+using CodeX.Data.Model;
+using CodeX.Web.Common;
+
+namespace CodeX.Muses.Web.ControlPanelHQ.Program
+{
+    public partial class ProsepectiveStudentFormCtl : BaseViewPopupCtl
+    {
+        protected int PageCount = 1;
+        public override void InitializeDataControl(string param)
+        {
+            hdnLocationID.Value = param;
+            Location entity = BusinessLayer.GetLocation(Convert.ToInt32(hdnLocationID.Value));
+            txtLocationCode.Text = entity.LocationCode;
+            txtLocationName.Text = entity.LocationName;
+
+            BindGridView(1, true, ref PageCount);
+
+            txtItemCode.Attributes.Add("validationgroup", "mpEntryPopup");
+            txtMinimum.Attributes.Add("validationgroup", "mpEntryPopup");
+            txtMaximum.Attributes.Add("validationgroup", "mpEntryPopup");
+        }
+
+        protected string OnGetItemProductFilterExpression()
+        {
+            return string.Format("GCItemType = '{1}' AND ItemID NOT IN (SELECT ItemID FROM ItemBalance WHERE LocationID = {0} AND IsDeleted = 0) AND IsDeleted = 0", hdnLocationID.Value, Constant.ItemType.PRODUCT);
+        }
+
+        private void BindGridView(int pageIndex, bool isCountPageCount, ref int pageCount)
+        {
+            string filterExpression = string.Format("LocationID = {0} AND IsDeleted = 0 AND ItemName1 LIKE '%{1}%'", hdnLocationID.Value, hdnFilterParam.Value);
+            if (isCountPageCount)
+            {
+                int rowCount = BusinessLayer.GetvItemBalanceRowCount(filterExpression);
+                pageCount = Helper.GetPageCount(rowCount, 8);
+            }
+
+            List<vItemBalance> lstEntity = BusinessLayer.GetvItemBalanceList(filterExpression, 8, pageIndex, "ItemName1 ASC");
+            lvwView.DataSource = lstEntity;
+            lvwView.DataBind();
+
+            //lvwView.DataSource = BusinessLayer.GetvItemBalanceList(string.Format("LocationID = {0} AND IsDeleted = 0 ORDER BY ItemName1 ASC", hdnLocationID.Value));
+            //lvwView.DataBind();
+        }
+
+        protected void grdView_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.Header)
+            {
+                for (int i = 0; i < e.Row.Cells.Count; i++)
+                    e.Row.Cells[i].Text = GetLabel(e.Row.Cells[i].Text);
+            }
+
+        }
+
+        protected void cbpEntryPopupView_Callback(object sender, DevExpress.Web.ASPxClasses.CallbackEventArgsBase e)
+        {
+            LoadWords();
+
+            int pageCount = 1;
+
+            string[] param = e.Parameter.Split('|');
+
+            string result = param[0] + "|";
+            string errMessage = "";
+
+            if (param[0] == "changepage")
+            {
+                BindGridView(Convert.ToInt32(param[1]), false, ref pageCount);
+                result = "changepage";
+            }
+            else if (param[0] == "refresh")
+            {
+                BindGridView(1, true, ref pageCount);
+                result = string.Format("refresh|{0}", pageCount);
+            }
+            else
+            {
+                if (param[0] == "save")
+                {
+                    if (hdnID.Value.ToString() != "")
+                    {
+                        if (OnSaveEditRecord(ref errMessage))
+                            result += "success";
+                        else
+                            result += string.Format("fail|{0}", errMessage);
+                    }
+                    else
+                    {
+                        if (OnSaveAddRecord(ref errMessage))
+                            result += "success";
+                        else
+                            result += string.Format("fail|{0}", errMessage);
+                    }
+                }
+                else if (param[0] == "delete")
+                {
+                    if (OnDeleteRecord(ref errMessage))
+                        result += "success";
+                    else
+                        result += string.Format("fail|{0}", errMessage);
+                }
+
+                BindGridView(1, true, ref pageCount);
+                result += "|" + pageCount;
+            }
+
+            ASPxCallbackPanel panel = sender as ASPxCallbackPanel;
+            panel.JSProperties["cpResult"] = result;
+        }
+
+        private void ControlToEntity(ItemBalance entity)
+        {
+            entity.ItemID = Convert.ToInt32(hdnItemID.Value);
+            entity.QuantityMIN = Convert.ToDecimal(txtMinimum.Text);
+            entity.QuantityMAX = Convert.ToDecimal(txtMaximum.Text);
+        }
+
+        private bool OnSaveAddRecord(ref string errMessage)
+        {
+            try
+            {
+                ItemBalance entity = new ItemBalance();
+                ControlToEntity(entity);
+                entity.LocationID = Convert.ToInt32(hdnLocationID.Value);
+                entity.CreatedBy = AppSession.UserLogin.UserID;
+                BusinessLayer.InsertItemBalance(entity);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                errMessage = ex.Message;
+                return false;
+            }
+        }
+
+        private bool OnSaveEditRecord(ref string errMessage)
+        {
+            try
+            {
+                ItemBalance entity = BusinessLayer.GetItemBalance(Convert.ToInt32(hdnID.Value));
+                ControlToEntity(entity);
+                entity.LastUpdatedBy = AppSession.UserLogin.UserID;
+                BusinessLayer.UpdateItemBalance(entity);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                errMessage = ex.Message;
+                return false;
+            }
+        }
+
+        private bool OnDeleteRecord(ref string errMessage)
+        {
+            try
+            {
+                ItemBalance entity = BusinessLayer.GetItemBalance(Convert.ToInt32(hdnID.Value));
+                entity.IsDeleted = true;
+                entity.LastUpdatedBy = AppSession.UserLogin.UserID;
+                BusinessLayer.UpdateItemBalance(entity);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                errMessage = ex.Message;
+                return false;
+            }
+        }
+    }
+}
