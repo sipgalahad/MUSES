7a2be72dc55cc126110b871401af53f56d0c0886
qis-agi <qis-agi@medinfras.com>
2015-03-26 12:34:15 +0700

    Add Rapor Mid Semester

---
+++ CodeX.Muses.Web.StudentManagement/Report/VID_RCI/StudentManagement/BRaporMidSemesterRpt.ascx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using CodeX.Web.Common.UI;
+using CodeX.Data.Model;
+using System.Web.UI.HtmlControls;
+using CodeX.Web.Common;
+using CodeX.Common;
+
+namespace CodeX.Muses.Web.StudentManagement.Report
+{
+    public partial class BRaporMidSemesterRpt : BaseCustomReportCtl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        //List<StudentFee> lstRegFee = null;
+
+        public override void Bind(string filterExpression, string[] param)
+        {
+            
+        }
+
+        protected void rptPayment_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+            if (e.Item.ItemType == System.Web.UI.WebControls.ListItemType.AlternatingItem || e.Item.ItemType == System.Web.UI.WebControls.ListItemType.Item)
+            {
+                //vStudentFeeComp entity = (vStudentFeeComp)e.Item.DataItem;
+                //Repeater rptPaymentDt = (Repeater)e.Item.FindControl("rptPaymentDt");
+                //rptPaymentDt.DataSource = lstRegFee.Where(x => x.StudentFeeCompID == entity.StudentFeeCompID);
+                //rptPaymentDt.DataBind();
+            }
+        }
+    }
+}
