2fe1c0eaf9af98f98c7689d52d903d5ca74ea276
qis-agi <qis-agi@medinfras.com>
2015-04-08 00:58:01 +0700

    Rapor

--- CodeX.Muses.Web.StudentManagement/Program/StudentMark/SubjectMark/ClassStudentSubjectMarkList.aspx.designer.cs
+++ CodeX.Muses.Web.StudentManagement/Program/StudentMark/SubjectMark/ClassStudentSubjectMarkList.aspx.designer.cs
@@ -13,15 +13,6 @@ namespace CodeX.Muses.Web.StudentManagement.Program {
     public partial class ClassStudentSubjectMarkList {
         
         /// <summary>
-        /// btnPrint control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlGenericControl btnPrint;
-        
-        /// <summary>
         /// hdnID control.
         /// </summary>
         /// <remarks>
