c85f66c50c7b152ef64deb3cc19c0e510fd1a364
agireza <agireza@yahoo.com>
2015-07-30 13:05:01 +0700

    Fixing Email

--- CodeX.Muses.Web.ProjectManagement/Program/Proses/ProjectTask/ProjectTaskList.aspx.designer.cs
+++ CodeX.Muses.Web.ProjectManagement/Program/Proses/ProjectTask/ProjectTaskList.aspx.designer.cs
@@ -22,6 +22,15 @@ namespace CodeX.Muses.Web.ProjectManagement.Program {
         protected global::System.Web.UI.HtmlControls.HtmlInputHidden hdnID;
         
         /// <summary>
+        /// hdnSelectedValue control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputHidden hdnSelectedValue;
+        
+        /// <summary>
         /// hdnFilterExpression control.
         /// </summary>
         /// <remarks>
