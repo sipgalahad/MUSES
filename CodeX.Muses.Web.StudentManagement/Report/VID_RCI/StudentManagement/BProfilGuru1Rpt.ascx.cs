272cb9712ac9856d38a278e25da895f039308120
agireza <agireza@yahoo.com>
2015-11-05 11:49:23 +0700

    Fixing BProfilGuru1Rpt

--- CodeX.Muses.Web.StudentManagement/Report/VID_RCI/StudentManagement/BProfilGuru1Rpt.ascx.cs
+++ CodeX.Muses.Web.StudentManagement/Report/VID_RCI/StudentManagement/BProfilGuru1Rpt.ascx.cs
@@ -14,46 +14,122 @@ namespace CodeX.Muses.Web.StudentManagement.Report
 {
     public partial class BProfilGuru1Rpt : BaseCustomReportCtl
     {
+        vTeacherSubject ts = null;
+        List<vTeacherSubject> lstTeacheSubject = null;
+        List<vTransTeacherProfileDtItem> lstProfileAllItem = null;
         List<vTransTeacherProfileDtItem> lstProfileItem = null;
-
+        List<vEmployee> lstEmployee = null;
+        List<EmployeeAttendanceSummary> lstEAS = null;
+        
         protected void Page_Load(object sender, EventArgs e)
         {
             
         }
-        vTeacherSubject ts = null;
+        
         public override void Bind(string filterExpression, string[] param)
         {
-            String text = divPersonalityType.InnerHtml;
-            vTransTeacherProfileDt ttpdt = BusinessLayer.GetvTransTeacherProfileDtList(filterExpression)[0];
-            divRBHeader.InnerHtml = divRBHeader.InnerHtml.Replace("{TeacherName}", ttpdt.TeacherName);
-            divPersonalityType.InnerHtml = text.Replace("{PersonalityType}", ttpdt.PersonalityTypeName);
+            List<vTransTeacherProfileDt> lstTtpdt = BusinessLayer.GetvTransTeacherProfileDtList(filterExpression);
+            String lstTeacherID = String.Join(",", lstTtpdt.Select(x => x.TeacherID));
+            lstTeacheSubject = BusinessLayer.GetvTeacherSubjectList(String.Format("TeacherID IN ({0})", lstTeacherID));
+            lstProfileAllItem = BusinessLayer.GetvTransTeacherProfileDtItemList(String.Format("TransTeacherProfileDtID IN ({0})", String.Join(",", lstTtpdt.Select(x => x.ID))));
+            String lstEmployeeID = String.Join(",", lstTtpdt.Select(x => x.TeacherID));
+            lstEmployee = BusinessLayer.GetvEmployeeList(String.Format("EmployeeID IN ({0})", lstEmployeeID));
+            lstEAS = BusinessLayer.GetEmployeeAttendanceSummaryList(String.Format("EmployeeID IN ({0}) AND IsDeleted = 0", lstEmployeeID));
+            rptMainBody.DataSource = lstTtpdt;
+            rptMainBody.DataBind();
+        }
 
-            text = divPersonal.InnerHtml;
-            text = text.Replace("{TeacherName}", ttpdt.TeacherName);
-            text = text.Replace("{IQ}", ttpdt.IQScore.ToString());
-            text = text.Replace("{IQInPercentage}", GetIQScore(ttpdt.IQScore));
-            text = text.Replace("{Drive}", ttpdt.DScore.ToString("N"));
-            text = text.Replace("{Komunikasi}", ttpdt.KScore.ToString("N"));
-            text = text.Replace("{Loyalitas}", ttpdt.LScore.ToString("N"));
-            text = text.Replace("{Ketelitian}", ttpdt.TScore.ToString("N"));
-            text = text.Replace("{Konsistensi}", ttpdt.KonsScoreInPercentage.ToString("N"));
-            divPersonal.InnerHtml = text;
+        protected void rptMainBody_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+            if (e.Item.ItemType == System.Web.UI.WebControls.ListItemType.AlternatingItem || e.Item.ItemType == System.Web.UI.WebControls.ListItemType.Item)
+            {
+                vTransTeacherProfileDt ttpdt = e.Item.DataItem as vTransTeacherProfileDt;
+                HtmlGenericControl divPersonalityType = e.Item.FindControl("divPersonalityType") as HtmlGenericControl;
+                HtmlGenericControl divPersonal = e.Item.FindControl("divPersonal") as HtmlGenericControl;
+                HtmlGenericControl divPersonalDesc = e.Item.FindControl("divPersonalDesc") as HtmlGenericControl;
+                HtmlGenericControl divEmploymentStatus = e.Item.FindControl("divEmploymentStatus") as HtmlGenericControl;
+                HtmlGenericControl divEmployeeAttendanceSummary = e.Item.FindControl("divEmployeeAttendanceSummary") as HtmlGenericControl;
 
-            text = divPersonalDesc.InnerHtml;
-            text = text.Replace("{Adventages}", ttpdt.Advantages.Replace("<br>","<br/>"));
-            text = text.Replace("{Weakness}", ttpdt.Weakness.Replace("<br>", "<br/>"));
-            divPersonalDesc.InnerHtml = text;
+                Repeater rptReportBody = e.Item.FindControl("rptReportBody") as Repeater;
 
-            filterExpression = String.Format("TransTeacherProfileDtID = {0}",ttpdt.ID);
-            lstProfileItem = BusinessLayer.GetvTransTeacherProfileDtItemList(filterExpression);
+                String text = divPersonalityType.InnerHtml;
+                divPersonalityType.InnerHtml = text.Replace("{PersonalityType}", ttpdt.PersonalityTypeName);
+                vEmployee emp = lstEmployee.FirstOrDefault(x => x.EmployeeID == ttpdt.TeacherID);
 
-            ts = BusinessLayer.GetvTeacherSubjectList(String.Format("TeacherID = {0}",ttpdt.TeacherID)).FirstOrDefault();
-            
-            List<Variable> lstGroup = (from grp in lstProfileItem group grp by new { grp.TeacherProfileGroupID, grp.TeacherProfileGroupDisplayText } into NewGrp select new Variable { Code = NewGrp.Key.TeacherProfileGroupID.ToString(), Value = NewGrp.Key.TeacherProfileGroupDisplayText }).ToList();
-            rptReportBody.DataSource = lstGroup;
-            rptReportBody.DataBind();
+                if (emp != null)
+                {
+                    text = divEmploymentStatus.InnerHtml;
+                    if (emp.HiredDate.ToString(Constant.FormatString.DATE_FORMAT) != "01-Jan-1900")
+                        text = text.Replace("{HiredDate}", emp.HiredDate.ToString(Constant.FormatString.DATE_FORMAT));
+                    else
+                        text = text.Replace("{HiredDate}", "-");
+                    if (emp.TerminatedDate.ToString(Constant.FormatString.DATE_FORMAT) != "01-Jan-1900")
+                        text = text.Replace("{TerminatedDate}", emp.TerminatedDate.ToString(Constant.FormatString.DATE_FORMAT));
+                    else
+                        text = text.Replace("{TerminatedDate}", "-");
+                    text = text.Replace("{EmployeeStatus}", emp.EmployeeStatus);
+                    text = text.Replace("{NIK}", emp.EmployeeCode);
+                    divEmploymentStatus.InnerHtml = text;
+                }
+                else
+                {
+                    text = divEmploymentStatus.InnerHtml;
+                    text = text.Replace("{HiredDate}", "-");
+                    text = text.Replace("{TerminatedDate}", "-");
+                    divEmploymentStatus.InnerHtml = text;
+                }
 
-            
+                text = divPersonal.InnerHtml;
+                text = text.Replace("{TeacherName}", ttpdt.TeacherName);
+                text = text.Replace("{IQ}", ttpdt.IQScore.ToString());
+                text = text.Replace("{IQInPercentage}", GetIQScore(ttpdt.IQScore));
+                text = text.Replace("{Drive}", ttpdt.DScore.ToString("N"));
+                text = text.Replace("{Komunikasi}", ttpdt.KScore.ToString("N"));
+                text = text.Replace("{Loyalitas}", ttpdt.LScore.ToString("N"));
+                text = text.Replace("{Ketelitian}", ttpdt.TScore.ToString("N"));
+                text = text.Replace("{Konsistensi}", ttpdt.KonsScoreInPercentage.ToString("N"));
+                divPersonal.InnerHtml = text;
+
+                text = divPersonalDesc.InnerHtml;
+                text = text.Replace("{Adventages}", ttpdt.Advantages.Replace("<br>", "<br/>"));
+                text = text.Replace("{Weakness}", ttpdt.Weakness.Replace("<br>", "<br/>"));
+                divPersonalDesc.InnerHtml = text;
+
+                EmployeeAttendanceSummary eas = lstEAS.FirstOrDefault(x => x.EmployeeID == emp.EmployeeID);
+                if (eas != null)
+                {
+                    text = divEmployeeAttendanceSummary.InnerHtml;
+                    text = text.Replace("{EffectiveDays}", eas.EfectiveDays.ToString());
+                    text = text.Replace("{SickDays}", eas.SickDays.ToString());
+                    text = text.Replace("{PermitDays}", eas.PermitDays.ToString());
+                    text = text.Replace("{AlphaDays}", eas.AlphaDays.ToString());
+                    text = text.Replace("{EffectiveDaysInPercentage}", (Convert.ToInt32((eas.EfectiveDays / (Decimal)eas.WorkDays) * 100)).ToString());
+                    text = text.Replace("{SickDaysInPercentage}", (Convert.ToInt32((eas.SickDays / (Decimal)eas.WorkDays) * 100)).ToString());
+                    text = text.Replace("{PermitDaysInPercentage}", (Convert.ToInt32((eas.PermitDays / (Decimal)eas.WorkDays) * 100)).ToString());
+                    text = text.Replace("{AlphaDaysInPercentage}", (Convert.ToInt32((eas.AlphaDays / (Decimal)eas.WorkDays) * 100)).ToString());
+                    divEmployeeAttendanceSummary.InnerHtml = text;
+                }
+                else
+                {
+                    text = divEmployeeAttendanceSummary.InnerHtml;
+                    text = text.Replace("{EffectiveDays}", "-");
+                    text = text.Replace("{SickDays}", "-");
+                    text = text.Replace("{PermitDays}", "-");
+                    text = text.Replace("{AlphaDays}", "-");
+                    text = text.Replace("{EffectiveDaysInPercentage}", "-");
+                    text = text.Replace("{SickDaysInPercentage}", "-");
+                    text = text.Replace("{PermitDaysInPercentage}", "-");
+                    text = text.Replace("{AlphaDaysInPercentage}", "-");
+                    divEmployeeAttendanceSummary.InnerHtml = text;
+                }
+
+                lstProfileItem = lstProfileAllItem.Where(x => x.TransTeacherProfileDtID == ttpdt.ID).ToList();
+                ts = lstTeacheSubject.Where(x => x.TeacherID == ttpdt.TeacherID).FirstOrDefault();
+
+                List<Variable> lstGroup = (from grp in lstProfileItem group grp by new { grp.TeacherProfileGroupID, grp.TeacherProfileGroupDisplayText } into NewGrp select new Variable { Code = NewGrp.Key.TeacherProfileGroupID.ToString(), Value = NewGrp.Key.TeacherProfileGroupDisplayText }).ToList();
+                rptReportBody.DataSource = lstGroup;
+                rptReportBody.DataBind();
+            }
         }
 
         protected void rptReportBody_ItemDataBound(object sender, RepeaterItemEventArgs e)
@@ -67,15 +143,19 @@ namespace CodeX.Muses.Web.StudentManagement.Report
 
                 Variable group = e.Item.DataItem as Variable;
 
-                switch (group.Code) 
-                { 
-                    case "2" :
+                switch (group.Code)
+                {
+                    case "2":
+                        divHeader.Style.Remove("display");
+                        divHeader.InnerHtml = "B. KOMPETENSI PEDAGOGIK & PROFESIONAL";
+                        break;
+                    case "4": 
                         divHeader.Style.Remove("display");
                         divHeader.InnerHtml = "B. KOMPETENSI PEDAGOGIK & PROFESIONAL";
                         break;
                     case "5":
                         divHeader.Style.Remove("display");
-                        divHeader.InnerHtml = "B. KOMPETENSI PEDAGOGIK & PROFESIONAL"; 
+                        divHeader.InnerHtml = "B. KOMPETENSI PEDAGOGIK & PROFESIONAL";
                         break;
                     case "8":
                         divHeader.Style.Remove("display");
@@ -87,17 +167,33 @@ namespace CodeX.Muses.Web.StudentManagement.Report
                         tdHeaderPercentage.Width = "50%";
                         tdHeaderMutu.Visible = false;
                         break;
-                    default : 
-                        divHeader.Style.Add("display","none"); 
+                    case "13":
+                        divHeader.Style.Remove("display");
+                        divHeader.InnerHtml = "Pendapat dan Kesan Siswa Secara Terbuka terhadap Profil Kepribadian Guru";
+                        tdHeaderPercentage.InnerHtml = "Jawaban";
+                        tdHeaderPercentage.ColSpan = 2;
+                        tdHeaderPercentage.Width = "50%";
+                        tdHeaderMutu.Visible = false;
+                        break;
+                    case "14":
+                        divHeader.Style.Remove("display");
+                        divHeader.InnerHtml = "D. PENDAPAT DAN KESAN SECARA TERBUKA DARI KOLEGA :";
+                        tdHeaderPercentage.InnerHtml = "Jawaban";
+                        tdHeaderPercentage.ColSpan = 2;
+                        tdHeaderPercentage.Width = "50%";
+                        tdHeaderMutu.Visible = false;
+                        break;
+                    default:
+                        divHeader.Style.Add("display", "none");
                         break;
                 }
 
-                List<vTransTeacherProfileDtItem> lstTemp = lstProfileItem.Where(x => x.TeacherProfileGroupID == Convert.ToInt32(group.Code)).ToList();
+                List<vTransTeacherProfileDtItem> lstTemp = lstProfileItem.Where(x => x.TeacherProfileGroupID == Convert.ToInt32(group.Code)).OrderBy(s => s.DisplayOrder).ToList();
                 if (lstTemp.Count() == 1 && ts != null)
                 {
                     lstTemp[0].TeacherProfileItemName = lstTemp[0].TeacherProfileItemName.Replace("{SubjectName}", ts.SubjectName);
                 }
-                else 
+                else
                 {
                     lstTemp[0].TeacherProfileItemName = lstTemp[0].TeacherProfileItemName.Replace("{SubjectName}", "");
                 }
@@ -105,7 +201,7 @@ namespace CodeX.Muses.Web.StudentManagement.Report
                 rptGroupItem.DataSource = lstTemp;
                 rptGroupItem.DataBind();
                 HtmlTableRow trMutu = e.Item.FindControl("trMutu") as HtmlTableRow;
-                if (lstTemp.Count() > 1 && group.Code != "12")
+                if (lstTemp.Count() > 1 && group.Code != "12" && group.Code != "13" && group.Code != "14")
                 {
                     HtmlTableCell tdFinalScore = e.Item.FindControl("tdFinalScore") as HtmlTableCell;
                     HtmlTableCell tdQualityScore = e.Item.FindControl("tdQualityScore") as HtmlTableCell;
@@ -122,12 +218,12 @@ namespace CodeX.Muses.Web.StudentManagement.Report
                         FinalScore = (TotScore / QualityPercentage * 100);
                     }
                     tdFinalScore.InnerHtml = FinalScore.ToString("N2");
-                    if(Convert.ToInt32(group.Code) < 7)
+                    if (Convert.ToInt32(group.Code) < 7)
                         tdQualityScore.InnerHtml = GetMutu(FinalScore);
                     else
                         tdQualityScore.InnerHtml = GetPetaUmpanBalik(FinalScore);
                 }
-                else 
+                else
                 {
                     trMutu.Visible = false;
                 }
@@ -153,20 +249,20 @@ namespace CodeX.Muses.Web.StudentManagement.Report
                     percentage = entity.Score / entity.QualityPercentage * 100;
                     tdPercentage.InnerHtml = percentage.ToString("N2");
                 }
-                else 
+                else
                 {
                     tdPercentage.InnerHtml = entity.Remarks;
                 }
 
-                if (entity.TeacherProfileGroupID < 7 && entity.TeacherProfileGroupID != 12)
+                if (entity.TeacherProfileGroupID < 7 && entity.GCTeacherProfileMarkType != Constant.TeacherProfileMarkType.TEXT)
                 {
                     tdMutu.InnerHtml = GetMutu(percentage);
                 }
-                else if (entity.TeacherProfileGroupID != 12)
+                else if (entity.GCTeacherProfileMarkType == Constant.TeacherProfileMarkType.NUMBER)
                 {
                     tdMutu.InnerHtml = GetPetaUmpanBalik(percentage);
                 }
-                else 
+                else
                 {
                     tdPercentage.ColSpan = 2;
                     tdPercentage.Align = "Left";
@@ -175,7 +271,7 @@ namespace CodeX.Muses.Web.StudentManagement.Report
             }
         }
 
-        protected String GetPetaUmpanBalik(decimal percentage) 
+        protected String GetPetaUmpanBalik(decimal percentage)
         {
             if (percentage > Convert.ToDecimal(4.1)) return "SB";
             else if (percentage > Convert.ToDecimal(3.34)) return "B";
@@ -183,17 +279,17 @@ namespace CodeX.Muses.Web.StudentManagement.Report
             else return "SK";
         }
 
-        protected String GetIQScore(Int32 iq) 
-        { 
-            if(iq > 129) return "Very Superior";
-            else if(iq > 119) return "Superior";
-            else if(iq > 109) return "High Average";
-            else if(iq > 89) return "Average";
+        protected String GetIQScore(Int32 iq)
+        {
+            if (iq > 129) return "Very Superior";
+            else if (iq > 119) return "Superior";
+            else if (iq > 109) return "High Average";
+            else if (iq > 89) return "Average";
             else if (iq > 79) return "Low Average";
             else return "Extremely Low";
         }
 
-        protected String GetMutu(decimal percentage) 
+        protected String GetMutu(decimal percentage)
         {
             if (percentage >= 80 && percentage <= 100) return "Amat Baik";
             else if (percentage >= 65 && percentage <= 79) return "Baik";
